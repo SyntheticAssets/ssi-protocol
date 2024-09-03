625f183724f80a027ea839437d926acd75e00945
east <bestsrc@qq.com>
2024-09-03 12:02:31 +0800

    update license

--- src/AssetIssuer.sol
+++ src/AssetIssuer.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.25;
 import "./Interface.sol";
 import {AssetController} from "./AssetController.sol";
--- src/Swap.sol
+++ src/Swap.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.25;
 import './Interface.sol';
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
