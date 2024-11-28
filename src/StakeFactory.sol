67cd83731f2d37f7e0ef33194cfc7c7addeef629
east <bestsrc@qq.com>
2024-11-28 18:11:18 +0800

    add getStakeTokens

--- src/StakeFactory.sol
+++ src/StakeFactory.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.25;
 import './Interface.sol';
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import './StakeToken.sol';
 import "forge-std/console.sol";
 
 contract StakeFactory is Ownable {
+    using EnumerableSet for EnumerableSet.UintSet;
     address public factoryAddress;
     mapping(uint256 => address) public stakeTokens;
+    EnumerableSet.UintSet private assetIDs;
 
     event CreateStakeToken(address stakeToken, uint256 assetID, uint48 cooldown);
 
@@ -17,7 +20,7 @@ contract StakeFactory is Ownable {
     }
 
     function createStakeToken(uint256 assetID, uint48 cooldown) external onlyOwner returns (address stakeToken)  {
-        require(stakeTokens[assetID] == address(0), "stake token already exists");
+        require(!assetIDs.contains(assetID), "stake token already exists");
         IAssetFactory factory = IAssetFactory(factoryAddress);
         address assetToken = factory.assetTokens(assetID);
         require(assetToken != address(0), "asset token not exists");
@@ -30,6 +33,15 @@ contract StakeFactory is Ownable {
             cooldown
         ));
         stakeTokens[assetID] = stakeToken;
+        assetIDs.add(assetID);
         emit CreateStakeToken(stakeToken, assetID, cooldown);
+    }
+
+    function getStakeTokens() external view returns (uint256[] memory ids, address[] memory tokens) {
+        ids = assetIDs.values();
+        tokens = new address[](ids.length);
+        for (uint i = 0; i < tokens.length; i++) {
+            tokens[i] = stakeTokens[assetIDs.at(i)];
+        }
     }
 }
